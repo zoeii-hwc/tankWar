839fcdac3503a7ba9d2dff71b6b9bb54c4062a49
tuluber@hotmail.com <tuluber@hotmail.com>
2020-07-30 19:03:44 +0800

    the tank image

--- src/main/java/org/example/tankwar/GameClient.java
+++ src/main/java/org/example/tankwar/GameClient.java
@@ -1,7 +1,9 @@
 package org.example.tankwar;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 
 public class GameClient extends JComponent {
@@ -17,5 +19,11 @@ public class GameClient extends JComponent {
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
 		this.setPreferredSize(new Dimension(screenWidth, screenWidth));
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(new ImageIcon("assets\\images\\itankD.png").getImage(),
+				400,100,null);//x軸,y軸,observer
 	}
 }
--- src/main/java/org/example/tankwar/TankWar.java
+++ src/main/java/org/example/tankwar/TankWar.java
@@ -8,11 +8,13 @@ import javax.swing.JFrame;
 public class TankWar {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		
+		GameClient gameClient = new GameClient(1024,768);
 		frame.add(new GameClient());
 		frame.setTitle("TankWar");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();//排版方式
+		
+		gameClient.repaint(); //會呼叫paintComponent
 	}
 }
